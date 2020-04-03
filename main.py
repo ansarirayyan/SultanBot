4d8a544c2e0b1ab341235ab48185096229ce0fd0
ansarirayyan <29453512+ansarirayyan@users.noreply.github.com>
2020-04-03 09:52:14 -0700

    made some edits to profanity filter

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ async def on_message(message):
         await message.channel.send("In case of an investigation by a federal or local authority, I do not include myself in any groups mentioned or condone any of the actions or language used in this establishment. I am simply a third-party bystander.")
 
     if profanity.contains_profanity(message.content):
-        await message.channel.send("Ey Köpekler, do not mix with the wolves for that they eat the jackals.") # For the record, I am not of Turkish origin
+        await message.delete()
+        await message.channel.send(str(message.author) + ": " + profanity.censor(str(message.content)))
+        print(profanity.censor(str(message.content)))
+        await message.channel.send("Ey " + message.author.mention + ", do not mix with the wolves for that they eat the jackals.") # For the record, I am not of Turkish origin
 
 client.run('') # GET RID OF THIS BEFORE PUSHING!!!!!!!!!!!!!
